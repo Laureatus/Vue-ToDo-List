2a883bedbc2cad55379cee9125edcaea3d7e962d
Laureatus <lorin.fankhauser@gmail.com>
2022-10-20 08:52:52 +0200

    improvements

--- todolist/src/components/organism/TodoList.vue
+++ todolist/src/components/organism/TodoList.vue
@@ -5,24 +5,40 @@
       placeholder="Text"
       @keyup.enter="submit"
     />
-    <button type="submit" @click="submit">Add</button>
+    <div v-if="isEditing">
+        <button type="submit" @click="update">Update</button>
+    </div>
+    <div v-else>
+        <button type="submit" @click="submit">Add</button>
+    </div>
+    
     <ol>
         <li v-for="(todo, index) in todos">
             {{ todo }}
+            <button @click="edit(index, todo)">Edit</button>
             <button @click="remove(index)">X</button>
         </li>
     </ol>
+    <button @click="toggleDeleted">Show deleted ToDo's</button>
+    <div v-if="showDeleted">
+        <ol>
+            <li v-for="done in finished">
+                {{ done }}
+            </li>
+        </ol>
+    </div>
 </template>
 
 <script lang="ts">
-import AtomButton from '@/components/atom/Button/AtomButton.vue';
-import Text1 from '@/components/atom/Text/Text1.vue';
-import { validate } from '@babel/types';
 export default {
     data() {
         return {
-            todos: [''],
+            selectedIndex: null,
+            todos: [],
+            finished: ['Test', 'Done'],
             text: '',
+            isEditing: false,
+            showDeleted: false,
         }
     },
 
@@ -30,11 +46,31 @@ export default {
         submit() {
             this.todos.push(this.text);
             this.text = '';
-            
         },
         remove(index) { 
+            let deleted = this.todos[index];
             this.todos.splice(index, 1);
-        }
+            this.finished.push(deleted);
+        },
+        edit(index, todo) {
+            this.text = todo;
+            this.selectedIndex = index;
+            this.isEditing = true;
+        },
+        update() {
+            this.todos.splice(this.selectedIndex, 1, this.text);
+            this.isEditing = false;
+         },
+        toggleDeleted() {
+            if (this.showDeleted == true) {
+                this.showDeleted = false;
+            } else {
+                this.showDeleted = true;
+            }
+        },
     }
 }
 </script>
+<style>
+
+</style>
